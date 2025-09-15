533ca5f1563f14dd63218b9efcdfdb72a0564484
Anderson pereira martins <andersonpmartins2004@gmail.com>
2025-09-15 20:00:46 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,15 @@
 
 # Este script garante que as dependências sejam instaladas antes de iniciar o bot.
 
-# 1. Instala/atualiza as dependências do arquivo requirements.txt
-echo "--- Forçando a instalação das dependências ---"
+# 1. Instala as dependências básicas do arquivo requirements.txt
+echo "--- Instalando dependências básicas ---"
 python -m pip install -r requirements.txt
 
-# 2. Inicia o bot do Discord
-echo "--- Dependências instaladas. Iniciando o bot... ---"
+# 2. Força a atualização do yt-dlp para a versão mais recente
+echo "--- Forçando a atualização do yt-dlp ---"
+python -m pip install --upgrade yt-dlp
+
+# 3. Inicia o bot do Discord
+echo "--- Dependências prontas. Iniciando o bot... ---"
 python bot_discord.py
 
